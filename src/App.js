e82dd8b725388feae3a6005189bcce5cb6fac9a3
keerthi46 <keerthiraj143.kr@gmail.com>
2022-07-03 17:05:14 +0530

    update 2

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
   return (
     <div className="container">
       {Object.keys(formErrors).length === 0 && isSubmit ? (
-        <div className="ui message success">Signed in successfully</div>
+        <div className="success">Signed in successfully</div>
       ) : (
         // <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
         <pre></pre>
@@ -65,39 +65,59 @@ function App() {
         <h1>Login Form</h1>
         <div className="ui divider"></div>
         <div className="ui form">
-          <div className="field">
-            <label>Username</label>
-            <input
-              type="text"
-              name="username"
-              placeholder="Username"
-              value={formValues.username}
-              onChange={handleChange}
-            />
-          </div>
-          <p>{formErrors.username}</p>
-          <div className="field">
-            <label>Email</label>
-            <input
-              type="text"
-              name="email"
-              placeholder="Email"
-              value={formValues.email}
-              onChange={handleChange}
-            />
-          </div>
-          <p>{formErrors.email}</p>
-          <div className="field">
-            <label>Password</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={formValues.password}
-              onChange={handleChange}
-            />
-          </div>
-          <p>{formErrors.password}</p>
+          <table>
+            {/* <div className="field"> */}
+            <tr>
+              <td>
+                <label>Username</label>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  value={formValues.username}
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            {/* </div> */}
+            <p>{formErrors.username}</p>
+            {/* <div className="field"> */}
+            <tr>
+              <td>
+                <label>Email</label>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  value={formValues.email}
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            {/* </div> */}
+            <p>{formErrors.email}</p>
+            {/* <div className="field"> */}
+            <tr>
+              <td>
+                <label>Password</label>
+              </td>
+              <td>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={formValues.password}
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            {/* </div> */}
+            <p>{formErrors.password}</p>
+          </table>
           <button className="fluid ui button blue">Submit</button>
         </div>
       </form>
